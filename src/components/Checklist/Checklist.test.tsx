8290a1267060fc97769a713485c613b60dd18f98
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2019-08-11 20:12:32 +0530

    remove unnecessary describe blocks

--- src/components/Checklist/Checklist.test.tsx
+++ src/components/Checklist/Checklist.test.tsx
@@ -15,39 +15,35 @@ const checklistItems = [
   },
 ]
 
-describe(`check for operations on checklist`, () => {
-  it(`has rendered a default of 2 items`, () => {
-    const { getByTestId } = render(
-      <Checklist checklistItems={checklistItems} />
-    )
-    let ul = getByTestId('items-list')
-    expect(ul.children.length).toEqual(2)
-  })
+test(`has rendered a default of 2 items`, () => {
+  const { getByTestId } = render(<Checklist checklistItems={checklistItems} />)
+  let ul = getByTestId('items-list')
+  expect(ul.children.length).toEqual(2)
+})
 
-  it(`updates UI of checked item on toggling`, () => {
-    const { getByLabelText } = render(
-      <Checklist checklistItems={checklistItems} />
-    )
-    let firstItem = getByLabelText(checklistItems[0].text) as HTMLInputElement
-    fireEvent.click(firstItem)
-    expect(firstItem.checked).toBeTruthy()
+test(`updates UI of checked item on toggling`, () => {
+  const { getByLabelText } = render(
+    <Checklist checklistItems={checklistItems} />
+  )
+  let firstItem = getByLabelText(checklistItems[0].text) as HTMLInputElement
+  fireEvent.click(firstItem)
+  expect(firstItem.checked).toBeTruthy()
 
-    fireEvent.click(firstItem)
-    expect(firstItem.checked).toBeFalsy()
-  })
+  fireEvent.click(firstItem)
+  expect(firstItem.checked).toBeFalsy()
+})
 
-  it(`shows the checked item length`, () => {
-    const { getByTestId, getByLabelText } = render(
-      <Checklist checklistItems={checklistItems} />
-    )
-    let p = getByTestId('checked-items')
+test(`shows the checked item length`, () => {
+  const { getByTestId, getByLabelText } = render(
+    <Checklist checklistItems={checklistItems} />
+  )
+  let p = getByTestId('checked-items')
 
-    let firstItem = getByLabelText(checklistItems[0].text) as HTMLInputElement
-    fireEvent.click(firstItem)
-    expect(p.textContent).toContain('1 of 2')
+  let firstItem = getByLabelText(checklistItems[0].text) as HTMLInputElement
+  fireEvent.click(firstItem)
+  expect(p.textContent).toContain('1 of 2')
 
-    let secondItem = getByLabelText(checklistItems[1].text) as HTMLInputElement
-    fireEvent.click(secondItem)
-    expect(p.textContent).toContain('2 of 2')
-  })
+  let secondItem = getByLabelText(checklistItems[1].text) as HTMLInputElement
+  fireEvent.click(secondItem)
+  expect(p.textContent).toContain('2 of 2')
 })
